19feec286c1fb9eec74480a919aaded4a83a2fdd
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-05-23 21:11:19 -0400

    updated file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,10 +12,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(routes)
+app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooksDB", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/GoogleBooks");
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> Listening on port ${PORT}!`);
+app.listen(PORT, function() {
+  console.log(`🌎  ==> Now listening on PORT ${PORT}!`);
 });
