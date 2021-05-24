0067e2135b86993e77c3a9e6e33c4cc0a131bb7a
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-05-23 21:11:05 -0400

    added/updated files in client folder

---
+++ client/build/precache-manifest.f1246ba57578eab488716a48ccfb2c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d57c3963f88dd393c55a7549ea13f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cace5440f148f5d61fe",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "563812fdcfa95d36e049",
+    "url": "/static/js/2.dbbf1802.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.dbbf1802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cace5440f148f5d61fe",
+    "url": "/static/js/main.d5e996b4.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
